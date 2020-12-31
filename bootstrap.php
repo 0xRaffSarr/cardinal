dfe03ba2b8ddda8d67066fcf89c89717cbc03cb8
Raffaele Sarracino <raffaele56t@gmail.com>
2020-12-31 13:54:08 +0100

    Added bootstrap project

--- bootstrap.php
+++ bootstrap.php
@@ -1,5 +1,13 @@
 <?php
-//load cardinal helpers
-require_once __DIR__ .'/vendor/xraffsarr/cardinal-core/CardinalCore/Helper/functions.php';
+//load cardinal helpers function
+use Symfony\Component\Dotenv\Dotenv;
 
+require_once __DIR__ .'/vendor/xraffsarr/cardinal-core/CardinalCore/Helper/functions.php';
+// load composer autoload
 require_once __DIR__ .'/vendor/autoload.php';
+
+(new Dotenv())->load(__DIR__.'/.env');
+
+\Cardinal\App::create(env('APP_NAME'), env('APP_URL'));
+
+app()->bootProvider();
